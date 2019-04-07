918e0ecda4ba2ecec32c4c33ea2f46914bd1fdea
DMaxence <maduhoux@student.42.fr>
2019-04-07 11:09:26 +0200

    [~] change from adress invalid to login invalid

--- src/Components/FakeServer/asyncValidate.jsx
+++ src/Components/FakeServer/asyncValidate.jsx
@@ -1,11 +1,11 @@
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const asyncValidate = (values /*, dispatch */) => {
+const asyncValidate = (values) => {
   return sleep(1000).then(() => {
     // simulate server latency
-    if (['foo@foo.com', 'bar@bar.com'].includes(values.login)) {
+    if (['foo', 'bar'].includes(values.login)) {
       // eslint-disable-next-line no-throw-literal
-      throw { login: 'Email already Exists' }
+      throw { login: 'Login already Exists' }
     }
   })
 }
